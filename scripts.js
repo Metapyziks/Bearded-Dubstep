856859f70ee314fcb16d112a02f5d58738b5d54b
Xylometazol <Xylometazol@hotmail.com>
2015-05-25 17:47:09 +0200

    no idea

--- scripts.js
+++ scripts.js
@@ -43,25 +43,28 @@ function writeManyImgFor(amount, url)
 
 
 writeLine("James is a fetus");
-writeImgLine("http://i.imgur.com/ydR8Dch.png", "surprised");
+writeImg("http://i.imgur.com/ydR8Dch.png", "surprised");
+writeImgLine("http://i.imgur.com/4eoCFHS.png", "surprised");
 writeLine("<b>Just kidding..</b>");
 writeImg("http://i.imgur.com/ydR8Dch.png", "face");
 
 var numberOfFaces = 0;
-function randomFace()
+function randomFace(imageToReplace, imageA, imageB)
 {
-	var image = document.getElementById("face");
+	var image = document.getElementById(imageToReplace);
 	
 	numberOfFaces = numberOfFaces + 1;
 	
 	if(numberOfFaces%2 == 0)
 	{
-		image.setAttribute("src", "http://i.imgur.com/8WQA8A4.png");
+		image.setAttribute("src", imageA);
 	}
 	else
 	{
-		image.setAttribute("src", "http://i.imgur.com/RPzwsoE.png");
+		image.setAttribute("src", imageB);
 	}
 }
 
-window.setInterval(randomFace, 16.66);
+window.setInterval(randomFace, 300, "surprised", "http://i.imgur.com/8WQA8A4.png", "http://i.imgur.com/RPzwsoE.png");
+
+window.setInterval(randomFace, 777, "face", "http://i.imgur.com/ydR8Dch.png", "http://i.imgur.com/B6Ib0wC.png");
