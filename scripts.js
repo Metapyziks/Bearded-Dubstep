727592c2d34d34b6b5784442fa463ebb54b7b8e0
Xylometazol <Xylometazol@hotmail.com>
2015-05-25 16:20:03 +0200

    added function

--- scripts.js
+++ scripts.js
@@ -24,7 +24,17 @@ function writeImgLine(url)
 	newLine();
 }
 
+function writeManyImg(amount, url)
+{
+	if(amount>0)
+	{
+		writeImgLine(url);
+		writeManyImg(amount-1, url);
+	}
+}
+
 
 writeLine("James is a fetus");
 writeImgLine("http://i.imgur.com/ydR8Dch.png");
 writeLine("<b>Just kidding..</b>");
+writeManyImg(2, "http://i.imgur.com/8WQA8A4.png");
